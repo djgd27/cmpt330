600d5e3b4bc6e1d7dc3b0362252771abc48b76db
djgd27 <david.galindodelgado1@marist.edu>
2025-04-30 19:43:33 -0400

    setup tailwindcss and verifed its working

--- final/final-client-webpage/src/App.tsx
+++ final/final-client-webpage/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +27,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-500 text-white text-3xl font-bold">
+      Tailwind CSS is working!
+    </div>
     </>
   )
 }
